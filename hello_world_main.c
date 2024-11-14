3e24a832bc79c57f01fad08e00cfff2fbcc662e8
marcossouza1232 <100810347+marcossouza1232@users.noreply.github.com>
2024-11-14 14:37:59 -0300

    Add files via upload

---
+++ hello_world_main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "hal/mcpwm_types.h"
+#include "driver/mcpwm_timer.h"
+#include "driver/mcpwm_oper.h"
+#include "driver/mcpwm_cmpr.h"
+#include "driver/mcpwm_gen.h"
+#include "soc/clk_tree_defs.h"
+
+
+// definicoes
+#define TIMER_USADO 0
+#define RESOLUCAO 10000
+#define NUM_TICKS 5
+#define DUTY_TICK 2
+
+#define TRUE 1
+#define FALSE 0
+
+#define PA 12
+#define PB 14
+
+void app_main(void)
+{
+	// relogio
+	mcpwm_timer_config_t timer_0_cfg = {
+		.group_id = TIMER_USADO,
+		.clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+		.resolution_hz = RESOLUCAO,
+		.count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+		.period_ticks = NUM_TICKS	
+	};
+	mcpwm_timer_handle_t timer_0 = NULL;
+	mcpwm_new_timer(&timer_0_cfg, &timer_0);
+	printf("relogio configurado\n");
+	// operador
+	mcpwm_operator_config_t oper_0_cfg = {
+		.group_id = TIMER_USADO,
+		.flags.update_gen_action_on_tez = TRUE
+	};
+	mcpwm_oper_handle_t oper_0 = NULL;
+	mcpwm_new_operator(&oper_0_cfg, &oper_0);
+	mcpwm_operator_connect_timer(oper_0, timer_0);
+	printf("operador configurado\n");
+	
+	// comparador
+	mcpwm_comparator_config_t cmpr_0_cfg = {
+		.flags.update_cmp_on_tez = TRUE
+	};
+	mcpwm_cmpr_handle_t cmpr_0 = NULL;
+	mcpwm_new_comparator(oper_0, &cmpr_0_cfg, &cmpr_0);
+	mcpwm_comparator_set_compare_value(cmpr_0, DUTY_TICK);
+	
+	// gerador 0
+	mcpwm_generator_config_t gen_0_cfg = {
+		.gen_gpio_num = PA
+	};
+	mcpwm_gen_timer_event_action_t gen_0_time_action = {
+		.direction = MCPWM_TIMER_DIRECTION_UP,
+		.event = MCPWM_TIMER_EVENT_FULL,
+		.action = MCPWM_GEN_ACTION_HIGH
+	};
+	mcpwm_gen_compare_event_action_t gen_0_cmpr_action = {
+		.direction = MCPWM_TIMER_DIRECTION_UP,
+		.comparator = cmpr_0,
+		.action = MCPWM_GEN_ACTION_LOW
+	};
+	mcpwm_gen_handle_t gen_0 = NULL;
+	mcpwm_new_generator(oper_0, &gen_0_cfg, &gen_0);
+	mcpwm_generator_set_action_on_timer_event(gen_0, gen_0_time_action); 
+	mcpwm_generator_set_action_on_compare_event(gen_0, gen_0_cmpr_action);
+	
+	// gerador 1
+	mcpwm_generator_config_t gen_1_cfg = {
+		.gen_gpio_num = PB,
+		.flags.invert_pwm = TRUE
+	};
+	mcpwm_gen_handle_t gen_1 = NULL;
+	mcpwm_new_generator(oper_0, &gen_1_cfg, &gen_1);
+	mcpwm_generator_set_action_on_timer_event(gen_1, gen_0_time_action); 
+	mcpwm_generator_set_action_on_compare_event(gen_1, gen_0_cmpr_action);
+	
+	// tempo morto
+	
+	mcpwm_timer_enable(timer_0);
+	mcpwm_timer_start_stop(timer_0, MCPWM_TIMER_START_NO_STOP);
+	printf("relogio rodando, pwm gerado.\n");
+	
+	return;	
+}
